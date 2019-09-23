6bbea8886a0296a19403e09caf90377abe8c0cf5
yangxiaoxu <17857318857@163.com>
2019-09-23 15:13:38 +0800

    add a class to judge if A + B >C is true

---
+++ src/AplusBandC.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * 给定区间[-2的31次方, 2的31次方]内的3个整数A、B和C，请判断A+B是否大于C。
+ *
+ */
+public class AplusBandC {
+
+public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    //输入第1行给出正整数T(<=10)，是测试用例的个数。随后给出T组测试用例，每组占一行，顺序给出A、B和C。整数间以空格分隔。
+    int n = sc.nextInt();
+    boolean[] result = new boolean[n];
+    for (int i = 0; i < n; i++) {
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        if ((c - a) < b) result[i] = true;
+        else result[i] = false;
+    }
+    for (int i = 0; i < n; i++) {
+       // 对每组测试用例，在一行中输出“Case #X: true”如果A+B>C，否则输出“Case #X: false”，其中X是测试用例的编号（从1开始）。
+        System.out.println("Case #" + (i + 1) + ": " + result[i]);
+    }
+}
+}
+
